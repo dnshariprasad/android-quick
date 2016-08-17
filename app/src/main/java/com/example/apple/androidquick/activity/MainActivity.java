3aef85cf08455ba2cf7548bfca8381bccde43dff
Hari Prasad <dnshariprasad@gmail.com>
2016-08-17 20:17:23 +0530

    activity package added

--- app/src/main/java/com/example/apple/androidquick/activity/MainActivity.java
+++ app/src/main/java/com/example/apple/androidquick/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.apple.androidquick;
+package com.example.apple.androidquick.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.apple.androidquick.R;
 
 public class MainActivity extends AppCompatActivity {
 
